9e6e2e0c0fccb7d8fbe21b8d819e0e5e02b342c4
Matsew-uwu <randriantsoamatthieu@gmail.com>
2023-01-23 16:43:50 +0100

    Configuration application

--- AutoLib/Startup.cs
+++ AutoLib/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AutoLib.Models.Domain;
 
 namespace AutoLib
 {
