ed558a6e19503bfe5f983de775fdf46706bb217c
Matsew-uwu <randriantsoamatthieu@gmail.com>
2023-01-23 16:38:08 +0100

    Génération du domaine avec la BD

---
+++ AutoLib/Models/Domain/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutoLib.Models.Domain
+{
+    public partial class Client
+    {
+        public Client()
+        {
+            Reservation = new HashSet<Reservation>();
+            Utilise = new HashSet<Utilise>();
+        }
+
+        public int IdClient { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime? DateNaissance { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+
+        public virtual ICollection<Reservation> Reservation { get; set; }
+        public virtual ICollection<Utilise> Utilise { get; set; }
+    }
+}
